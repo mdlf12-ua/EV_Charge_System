0b1f29b05d366633f3d6182083e071e26d16d782
dsa39-ua <dsa39@alu.ua.es>
2025-10-03 17:14:56 +0200

    Añadido tabla de ChargingPoint

--- init.sql
+++ init.sql
@@ -1,1 +1,9 @@
-ID,Ubicacion,PRECIO,ESTADO
+CREATE TABLE ChargingPoint (
+    ID VARCHAR(20) PRIMARY KEY,
+    Ubicacion VARCHAR(100),
+    PRECIO DECIMAL(6,2),
+    ESTADO ENUM('Activado','Parado','Suministrando') NOT NULL,
+    CONDUCTOR_ID VARCHAR(20),
+    CONSUMO_KW DECIMAL(6,2),   
+    IMPORTE_EU DECIMAL(6,2)    
+);
